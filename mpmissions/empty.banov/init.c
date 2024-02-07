82b2cd7e2576ccebb5343f93545d7e773f15a213
TheFirstNoob <TheAssassin666@yandex.ru>
2024-02-07 23:42:08 +0300

    Update init.c
    - Удален лишний код.
    - Изменена дата ближе к текущей.

--- mpmissions/empty.banov/init.c
+++ mpmissions/empty.banov/init.c
@@ -1,13 +1,6 @@
 #include "$CurrentDir:mpmissions\empty.banov\Restart_System.c"
 
-//Vanilla Banov Init.c
-// Thank you for using my map
-	
-//Loot spawn Creator
-//GetCEApi().ExportProxyData(vector.Zero, 100000); //Loot
-//GetCEApi().ExportClusterData(); //Fruit	
-
-	void main()
+void main()
 {
 	//INIT ECONOMY--------------------------------------
 	Hive ce = CreateHive();
@@ -16,7 +9,7 @@
 
 	//DATE RESET AFTER ECONOMY INIT-------------------------
 	int year, month, day, hour, minute;
-	int reset_month = 9, reset_day = 20;
+	int reset_month = 2, reset_day = 7;
 	GetGame().GetWorld().GetDate(year, month, day, hour, minute);
 
 	if ((month == reset_month) && (day < reset_day))
@@ -41,15 +34,6 @@
 
 class CustomMission: MissionServer
 {
-	void SetRandomHealth(EntityAI itemEnt)
-	{
-		if ( itemEnt )
-		{
-			float rndHlt = Math.RandomFloat( 0.45, 0.65 );
-			itemEnt.SetHealth01( "", "", rndHlt );
-		}
-	}
-
 	override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
 	{
 		Entity playerEnt;
@@ -60,44 +44,14 @@ class CustomMission: MissionServer
 
 		return m_player;
 	}
-
-	override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
+	
+	override void InvokeOnConnect(PlayerBase player, PlayerIdentity identity)
 	{
-		EntityAI itemClothing;
-		EntityAI itemEnt;
-		ItemBase itemBs;
-		float rand;
-
-		itemClothing = player.FindAttachmentBySlotName( "Body" );
-		if ( itemClothing )
+		super.InvokeOnConnect(player, identity);
+		if (identity.GetName() == "Survivor")
 		{
-			SetRandomHealth( itemClothing );
-			
-			itemEnt = itemClothing.GetInventory().CreateInInventory( "BandageDressing" );
-			player.SetQuickBarEntityShortcut(itemEnt, 2);
-			
-			string chemlightArray[] = { "Chemlight_White", "Chemlight_Yellow", "Chemlight_Green", "Chemlight_Red" };
-			int rndIndex = Math.RandomInt( 0, 4 );
-			itemEnt = itemClothing.GetInventory().CreateInInventory( chemlightArray[rndIndex] );
-			SetRandomHealth( itemEnt );
-			player.SetQuickBarEntityShortcut(itemEnt, 1);
-
-			rand = Math.RandomFloatInclusive( 0.0, 1.0 );
-			if ( rand < 0.35 )
-				itemEnt = player.GetInventory().CreateInInventory( "Apple" );
-			else if ( rand > 0.65 )
-				itemEnt = player.GetInventory().CreateInInventory( "Pear" );
-			else
-				itemEnt = player.GetInventory().CreateInInventory( "Plum" );
-			player.SetQuickBarEntityShortcut(itemEnt, 3);
-			SetRandomHealth( itemEnt );
+		GetGame().DisconnectPlayer(identity);
 		}
-		
-		itemClothing = player.FindAttachmentBySlotName( "Legs" );
-		if ( itemClothing )
-			SetRandomHealth( itemClothing );
-		
-		itemClothing = player.FindAttachmentBySlotName( "Feet" );
 	}
 };
 
