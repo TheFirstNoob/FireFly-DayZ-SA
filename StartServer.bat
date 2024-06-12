ea690e4d67b219a1af54a1444b17fe4890b3e1eb
TheFirstNoob <TheAssassin666@yandex.ru>
2024-06-12 16:04:33 +0300

    update to latest

--- StartServer.bat
+++ StartServer.bat
@@ -1,12 +1,19 @@
 @echo off
 :start
-set serverName=DayZ Server
+::Server name
+set serverName=DayZ Test Server
+::Server files location
 set serverLocation="C:\Program Files (x86)\Steam\steamapps\common\DayZServer"
+::Server Port
 set serverPort=2302
+::Server config
 set serverConfig=serverDZ.cfg
-set modlist=@CF;@Dabs-Framework;@Community-Online-Tools;@Banov;@DayZ-Expansion-Licensed;@DayZ-Expansion-Bundle;@DayZ-Expansion-Animations;@InediaInfectedAI;@InediaStamina;@RaG-Immersive-Vehicles;@Tactical-Flava;@AmmoStackBullet;@RUSForma-vehicles;@Ambient-Animals-Pack;@Apokot-Fishing-Mod;@MuchStuffPack;@Talking-Bots;@A-6-Secure-Containers;@CannabisPlus;@DayZ-Mining-System;
+::Sets title for terminal (DONT edit)
+set modlist=@CF;@Dabs-Framework;@Community-Online-Tools;@Banov;@DayZ-Expansion-Licensed;@DayZ-Expansion-Bundle;@DayZ-Expansion-Animations;@FireFly-Main;@InediaInfectedAI;@InediaStamina;@RaG-Immersive-Vehicles;@Tactical-Flava;@RUSForma-vehicles;@Ambient-Animals-Pack;@Apokot-Fishing-Mod;@MuchFramework;@MuchStuffPack;@A-6-Secure-Containers;@CannabisPlus;@DayZ-Mining-System;@BBJ-Incredible-Creatures;@Harks-Blood-Plague;
 set servermodlist=@DayZ-Dynamic-AI-Addon;
 title %serverName% batch
+::DayZServer location (DONT edit)
 cd "%serverLocation%"
 echo (%time%) %serverName% started.
-start "DayZ Server" /min "DayZServer_x64.exe" -high -config=%serverConfig% -port=%serverPort% -profiles=profiles -mod=%modlist% -servermod=%servermodlist% -doLogs -cpuCount=6
+::Launch parameters (edit end: -config=|-port=|-profiles=|-doLogs|-adminLog|-netLog|-freezeCheck|-filePatching|-BEpath=|-cpuCount=)
+start "DayZ Server" /min "DayZServer_x64.exe" -high -config=%serverConfig% -port=%serverPort% -profiles=profiles -mod=%modlist% -servermod=%servermodlist% -doLogs -cpuCount=6 -limitFPS=500
