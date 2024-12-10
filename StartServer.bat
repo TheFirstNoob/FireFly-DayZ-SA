5c12f342b821587add7aad709c44961c4887643e
TheFirstNoob <TheAssassin666@yandex.ru>
2024-12-11 01:34:46 +0300

    Remove Apokot Fishing mod

--- StartServer.bat
+++ StartServer.bat
@@ -9,7 +9,7 @@ set serverPort=2303
 ::Server config
 set serverConfig=serverDZ.cfg
 ::Sets title for terminal (DONT edit)
-set modlist=@CF;@Dabs-Framework;@Community-Online-Tools;@BanovFrost;@DayZ-Expansion-Licensed;@DayZ-Expansion-Bundle;@DayZ-Expansion-Animations;@InediaInfectedAI;@InediaStamina;@RaG-Immersive-Vehicles;@Tactical-Flava;@RUSForma-vehicles;@Apokot-Fishing-Mod;@MuchFramework;@MuchStuffPack;@A-6-Secure-Containers;@CannabisPlus;@DayZ-Mining-System;@FireFly-Main;
+set modlist=@CF;@Dabs-Framework;@Community-Online-Tools;@BanovFrost;@DayZ-Expansion-Licensed;@DayZ-Expansion-Bundle;@DayZ-Expansion-Animations;@InediaInfectedAI;@InediaStamina;@RaG-Immersive-Vehicles;@Tactical-Flava;@RUSForma-vehicles;@MuchFramework;@MuchStuffPack;@A-6-Secure-Containers;@CannabisPlus;@DayZ-Mining-System;@FireFly-Main;
 set servermodlist=@DayZ-Dynamic-AI-Addon;@Server-PVE-SYSTEM;
 title %serverName% batch
 ::DayZServer location (DONT edit)
