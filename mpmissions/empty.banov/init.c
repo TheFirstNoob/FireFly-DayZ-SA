143e74b06761d5ea29836438ae88a5edfc4179a4
TheFirstNoob <TheAssassin666@yandex.ru>
2025-03-30 15:02:54 +0300

    Sync files for Summer Banov

---
+++ mpmissions/empty.banov/init.c
@@ -0,0 +1,51 @@
+#include "$CurrentDir:mpmissions\empty.banov\Restart_System.c"
+
+void main()
+{
+	Hive ce = CreateHive();
+	if ( ce )
+		ce.InitOffline();
+
+	int year, month, day, hour, minute;
+	int reset_month = 3, reset_day = 29;
+	GetGame().GetWorld().GetDate(year, month, day, hour, minute);
+
+	if ((month == reset_month) && (day < reset_day))
+	{
+		GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+	}
+	else
+	{
+		if ((month == reset_month + 1) && (day > reset_day))
+		{
+			GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+		}
+		else
+		{
+			if ((month < reset_month) || (month > reset_month + 1))
+			{
+				GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+			}
+		}
+	}
+}
+
+class CustomMission: MissionServer
+{
+	override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
+	{
+		Entity playerEnt;
+		playerEnt = GetGame().CreatePlayer( identity, characterName, pos, 0, "NONE" );
+		Class.CastTo( m_player, playerEnt );
+
+		GetGame().SelectPlayer( identity, m_player );
+
+		return m_player;
+	}
+};
+
+Mission CreateCustomMission(string path)
+{
+	return new CustomMission();
+}	
+	
