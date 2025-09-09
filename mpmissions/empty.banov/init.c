5176193e7dfcb36530e2503e6ee69a2320de6e15
TheFirstNoob <TheAssassin666@yandex.ru>
2025-09-09 11:55:54 +0300

    SYNC ALL FILES #2

--- mpmissions/empty.banov/init.c
+++ mpmissions/empty.banov/init.c
@@ -7,7 +7,7 @@ void main()
 		ce.InitOffline();
 
 	int year, month, day, hour, minute;
-	int reset_month = 5, reset_day = 23;
+	int reset_month = 8, reset_day = 31;
 	GetGame().GetWorld().GetDate(year, month, day, hour, minute);
 
 	if ((month == reset_month) && (day < reset_day))
